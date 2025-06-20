2da4ded5a09337921f8fc447a853274ca2c8d1e7
nduka-vic <vic.nduka@gmail.com>
2025-06-20 18:19:39 +0100

    updated supabaseClient config

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,6 @@
 // src/lib/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = "https://dkqxfylmpabsmmmoyuwj.supabase.co";
-// const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrcXhmeWxtcGFic21tbW95dXdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0MjE1MDEsImV4cCI6MjA2NTk5NzUwMX0._GN_SNAN028a7mrnyxiRbFEHBxVxpLY6qxRH6Xm1eeQ";
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
